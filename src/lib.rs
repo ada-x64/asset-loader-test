ef9ad482bcf9fa4193fd51b3d7df80ce42b896b4
ada <ada@cubething.dev>
2026-01-28 18:38:02 -0600

    further minimize

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 #[cfg(test)]
 mod test {
-    use bevy::{
-        app::ScheduleRunnerPlugin, diagnostic::FrameCountPlugin, log::LogPlugin, prelude::*,
-        state::app::StatesPlugin, time::TimePlugin,
-    };
+    use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*};
 
     #[derive(Resource, Default)]
     struct TheHandle(Handle<Image>);
@@ -12,13 +9,10 @@ mod test {
         let mut app = App::new();
         app.add_plugins((
             TaskPoolPlugin::default(),
-            FrameCountPlugin,
-            TimePlugin,
             ScheduleRunnerPlugin::default(),
             LogPlugin::default(),
             AssetPlugin::default(),
             ImagePlugin::default(),
-            StatesPlugin,
         ));
         app.init_resource::<TheHandle>();
         app.add_systems(
@@ -44,9 +38,10 @@ mod test {
                         warn!("Asset not started to load.");
                     }
                     bevy::asset::LoadState::Loading => {
-                        info!("loading...");
+                        info_once!("loading...");
                     }
                     bevy::asset::LoadState::Loaded => {
+                        info!("Loaded!");
                         commands.write_message(AppExit::Success);
                     }
                     bevy::asset::LoadState::Failed(asset_load_error) => {
